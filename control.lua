d2ef713ab0c6d968ffda1783f486cedfbecdca20
Harag <harag@cortexx.net>
2022-10-03 15:37:52 +0200

    move configuration stuff to the bottom

--- control.lua
+++ control.lua
@@ -7,28 +7,16 @@ local format = string.format
 
 message_level = tonumber(settings.global["ltn-interface-console-level"].value)
 
-local function check_delivery_reset_setting(report_to)
-  	if settings.global["ltn-dispatcher-requester-delivery-reset"].value then
-		report_to.print({ "se-ltn-glue-message.requester-delivery-reset-should-be-disabled", { "mod-setting-name.ltn-dispatcher-requester-delivery-reset"} })
-	end
+local function initialize()
 end
 
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
-	if not event then return end
-
-  	if event.setting == "ltn-interface-console-level" then
-    	message_level = tonumber(settings.global["ltn-interface-console-level"].value)
-  	elseif event.setting == "ltn-dispatcher-requester-delivery-reset" then
-		check_delivery_reset_setting(game)
-	end
-end)
-
-local function add_closest_elevator_to_schedule(entity, schedule_records)
+local function add_closest_elevator_to_schedule(entity, schedule_records, surface_connections)
 	local found_stop = nil
 	local distance = 2147483647 -- maxint
 
 	-- TODO could be optimized with a lookup per surface but there aren't that many per surface
 	-- TODO make use of new surface_connections field in event
+
 	for _, elevator_stop in pairs(entity.surface.find_entities_filtered { name = "se-space-elevator-train-stop" }) do
 		local stop_distance = get_distance_squared(elevator_stop.position, entity.position)
 		if (not found_stop) or (stop_distance < distance) then
@@ -82,13 +70,13 @@ local function register_event_handlers()
 		for _, record in pairs(event.train.schedule.records) do
 			if record.station == event.from and loco.surface ~= from_stop.surface then
 				-- different surfaces implies no temp-stop before this record to consider
-				add_closest_elevator_to_schedule(loco, new_records)
+				add_closest_elevator_to_schedule(loco, new_records, event.surface_connections)
 				passage_count = passage_count + 1
 			end
 
 			if record.station == event.to and from_stop.surface ~= to_stop.surface then
 				-- different surfaces implies no temp-stop before this record to consider
-				add_closest_elevator_to_schedule(from_stop, new_records)
+				add_closest_elevator_to_schedule(from_stop, new_records, event.surface_connections)
 				passage_count = passage_count + 1
 			end
 
@@ -96,7 +84,7 @@ local function register_event_handlers()
 
 			if record.station == event.to and passage_count < 2 then
 				-- train is not yet on the surface it came from
-				add_closest_elevator_to_schedule(to_stop, new_records)
+				add_closest_elevator_to_schedule(to_stop, new_records, event.surface_connections)
 				passage_count = passage_count + 1
 			end
 		end
@@ -108,8 +96,20 @@ local function register_event_handlers()
 	end)
 end
 
-script.on_init(register_event_handlers)
-script.on_load(register_event_handlers)
+script.on_init(function() 
+	initialize()
+	register_event_handlers()
+end)
+
+script.on_load(function()
+	register_event_handlers()
+end)
+
+local function check_delivery_reset_setting(report_to)
+	if settings.global["ltn-dispatcher-requester-delivery-reset"].value then
+		report_to.print({ "se-ltn-glue-message.requester-delivery-reset-should-be-disabled", { "mod-setting-name.ltn-dispatcher-requester-delivery-reset"} })
+    end
+end
 
 script.on_event(defines.events.on_player_created, function(event)
 	local player = game.get_player(event.player_index)
@@ -126,5 +126,16 @@ script.on_event(defines.events.on_player_joined_game, function(event)
 end)
 
 script.on_configuration_changed(function()
+	initialize()
 	check_delivery_reset_setting(game)
+end)
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+	if not event then return end
+
+	if event.setting == "ltn-interface-console-level" then
+		message_level = tonumber(settings.global["ltn-interface-console-level"].value)
+	elseif event.setting == "ltn-dispatcher-requester-delivery-reset" then
+		check_delivery_reset_setting(game)
+	end
 end)
